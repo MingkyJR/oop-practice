4436262b94d358d2805b74a9b80021f82fe9f07c
MingkyJR <proof0503@gmail.com>
2023-12-03 02:48:05 +0900

    c

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class User {
+    private String password;
+
+    public void initPassword(PasswordGenerator passwordGenerator) {
+        //as-is
+        //RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+
+        //to-be
+        String randomPassword = passwordGenerator.generatePassword();
+
+        if(randomPassword.length() >= 8 && randomPassword.length() <= 12) {
+            this.password = randomPassword;
+        }
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
